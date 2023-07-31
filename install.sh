1826e8f479f2a0f27d051c8a0f9fe89d1e0b635a
strongcourage <nguyenmanhdung1710@gmail.com>
2023-07-31 09:45:43 +0000

    add installation script, update correct versions of DL Python libraries

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Ubuntu 20.04.6 LTS - focal
+#git clone https://github.com/Montimage/maip.git
+#cd maip
+
+# Install some 
+sudo apt-get update -y
+sudo apt install -y git wget cmake gcc g++ cpp curl software-properties-common
+
+# Install Python ML libraries (Python 3.8.10, pip 20.0.2)
+sudo apt install -y python3-pip graphviz
+pip3 install src/server/deep-learning/requirements.txt
+
+# Install mmt tools
+sudo apt install -y libconfuse-dev libpcap-dev libxml2-dev net-tools
+sudo ldconfig
+sudo dpkg -i src/server/mmt-packages/mmt-dpi*.deb
+sudo dpkg -i src/server/mmt-packages/mmt-security*.deb
+sudo dpkg -i src/server/mmt-packages/mmt-probe*.deb 2>/dev/null||true
+
+# Install nodejs v19.9.0
+sudo apt-get update -y
+curl -sL https://deb.nodesource.com/setup_19.x | bash
+sudo apt install -y nodejs
+
+# Install server
+npm install
+cp env.example .env
+
+# Run server
+# ./start-maip.sh
+
+# Install client
+cd src/client
+npm install # --force
+cd ../../
+
+# Run client
+# ./start-client.sh
