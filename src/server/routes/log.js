cc2e1a4094b1e44b2d61bdb54d8a55adfffc7d1b
strongcourage <nguyenmanhdung1710@gmail.com>
2023-09-06 07:59:02 +0000

    add DELETE /logs

--- src/server/routes/log.js
+++ src/server/routes/log.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 const {
   LOG_PATH,
@@ -8,13 +9,31 @@ const {
 const {
   listFiles, readTextFile, isFileExist,
 } = require('../utils/file-utils');
-/* GET all pcap files */
+
 router.get('/', (req, res, next) => {
   listFiles(LOG_PATH, '.log', (files) => {
     res.send({
       logs: files,
     });
   });
+});
+
+router.delete('/', (req, res, next) => {
+  try {
+    listFiles(LOG_PATH, '.log', (files) => {
+      files.forEach(file => {
+        const filePath = path.join(LOG_PATH, file);
+        fs.unlinkSync(filePath);
+      });
+
+      res.send({
+        message: 'All .log files deleted successfully'
+      });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 router.get('/:fileName/download', (req, res, next) => {
