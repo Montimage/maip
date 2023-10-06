80b3a7111c5b2ce5a8adb861c6bceff3d506a02e
strongcourage <nguyenmanhdung1710@gmail.com>
2023-10-06 20:34:55 +0000

    fix Docker server

--- src/server/constants.js
+++ src/server/constants.js
@@ -1,12 +1,13 @@
-const LOG_PATH = `${__dirname}/logs/`;
-const DEFAULT_LOG_PATH = `${__dirname}/logs/all-logs.log`;
-const MMT_PATH = `${__dirname}/mmt/`;
+const BASE_PATH = process.env.DOCKER_ENV ? '/maip-app/src/server' : __dirname;
+const LOG_PATH = `${BASE_PATH}/logs/`;
+const DEFAULT_LOG_PATH = `${BASE_PATH}/logs/all-logs.log`;
+const MMT_PATH = `${BASE_PATH}/mmt/`;
 const REPORT_PATH = `${MMT_PATH}outputs/`;
 const PCAP_PATH = `${MMT_PATH}pcaps/`;
 const MMT_PROBE_CONFIG_PATH = `${MMT_PATH}mmt-probe.conf`;
 const PCAP_EXTENSIONS = ['.pcap', '.pcapng', '.cap'];
 // Deep learning constants paths
-const DEEP_LEARNING_PATH = `${__dirname}/deep-learning/`;
+const DEEP_LEARNING_PATH = `${BASE_PATH}/deep-learning/`;
 const MODEL_PATH = `${DEEP_LEARNING_PATH}models/`;
 const PREDICTION_PATH = `${DEEP_LEARNING_PATH}predictions/`;
 const TRAINING_PATH = `${DEEP_LEARNING_PATH}trainings/`;
@@ -15,7 +16,7 @@ const ATTACKS_PATH = `${DEEP_LEARNING_PATH}attacks/`;
 const DATASETS_PATH = `${DEEP_LEARNING_PATH}datasets/`;
 const PYTHON_CMD = `python3`;
 
-const AC_PATH = `${__dirname}/activity-classification/`;
+const AC_PATH = `${BASE_PATH}/activity-classification/`;
 const AC_TRAINING_PATH = `${AC_PATH}trainings/`;
 
 const OUTPUT_DIRS = [TRAINING_PATH, PREDICTION_PATH, XAI_PATH, ATTACKS_PATH];
--- start-maip.sh
+++ start-maip.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# create the output directories if they don't exist
+mkdir -p src/server/deep-learning/attacks
+mkdir -p src/server/deep-learning/models
+mkdir -p src/server/deep-learning/trainings
+mkdir -p src/server/deep-learning/predictions
+mkdir -p src/server/deep-learning/xai
+mkdir -p src/server/mmt/outputs
+
 npm start
